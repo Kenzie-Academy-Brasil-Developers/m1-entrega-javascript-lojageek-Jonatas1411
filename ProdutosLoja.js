b41f2e7fd6d58ad66ce3b53db0f6471e6c64ff24
Jonatas Andrade <106789706+Jonatas1411@users.noreply.github.com>
2022-07-11 22:38:07 -0300

    Add files via upload

---
+++ ProdutosLoja.js
@@ -0,0 +1,46 @@
+let itensDaLoja = [
+
+{
+    id: 0,
+    nome: "Anime Woman",
+    catergoria: "actions",
+    imagem: "assets\img\actions\animewoman.jpg",
+    valor: "R$ 109,69",
+
+},
+
+{
+    id: 1,
+    nome: "Goku SSJ",
+    catergoria: "actions",
+    imagem: "assets\img\actions\dragonballpersonagem.jpg",
+    valor: "R$ 249,90"
+},
+
+
+{
+    id: 2,
+    nome: "C3PO StarWars",
+    catergoria: "actions",
+    imagem: "assets\img\actions\starwarspersonagem.jpg",
+    valor: "R$ 155,39",
+},
+
+{
+    id: 3,
+    nome: "Relógio",
+    catergoria: "painting",
+    imagem:"assets\img\painting\clock.jpg",
+    valor: "R$ 110,00",
+
+},
+
+{
+    id: 4,
+    nome: "Gamepad",
+    catergoria: "painting",
+    imagem: "assets\img\painting\gamepad.jpg",
+    valor: "R$ 133,38", 
+},
+ 
+]    
